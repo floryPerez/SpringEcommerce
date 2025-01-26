dc2a76b8557e855cce05d0fbe2a04a1cc39a40d4
floryPerez <51434291+floryPerez@users.noreply.github.com>
2025-01-26 11:08:47 -0600

    funcionalidad de quitar del carrito

--- Documents/GitHub/Spring-ecommerce/src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ Documents/GitHub/Spring-ecommerce/src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.curso.ecommerce.controller;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
@@ -67,15 +68,16 @@ public class HomeController {
 		log.info("Producto añadido: {}", optionalProducto.get());
 		log.info("Cantidad:{}", cantidad);
 		producto = optionalProducto.get();
+
 		detalleOrden.setCantidad(cantidad);
 		detalleOrden.setPrecio(producto.getPrecio());
 		detalleOrden.setNombre(producto.getNombre());
 		detalleOrden.setTotal(producto.getPrecio() * cantidad);
 		detalleOrden.setProducto(producto);
 
-		//añadir cada detalle a la lista 
+		// añadir cada detalle a la lista
 		detalles.add(detalleOrden);
-		
+
 		sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
 
 		orden.setTotal(sumaTotal);
@@ -84,6 +86,35 @@ public class HomeController {
 		model.addAttribute("cart", detalles);
 		model.addAttribute("orden", orden);// recibir en la vista esas variables y mostrar información
 		return "usuario/carrito";
+	}
+
+	// quitar un producto del carrito
+	@GetMapping("delete/cart/id")
+	public String deleteProductoCart(@PathVariable Integer id, Model model) {
+
+		// crear lista nueva de productos
+		List<DetalleOrden> ordenesNueva = new ArrayList<DetalleOrden>();
+
+		for (DetalleOrden detalleOrden : detalles) {
+			if (detalleOrden.getProducto().getId() != id) {
+				ordenesNueva.add(detalleOrden);
+			}
+		}
+//poner la nueva lista con los prod restantes
+
+		detalles = ordenesNueva;
+
+		double sumaTotal = 0;
+
+		sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
+//se pone el total de la orden
+		orden.setTotal(sumaTotal);
+		// se manda el total las ordenes
+
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);// recibir en la vista esas variables y mostrar información
+		return "usuario/carrito";
+
 	}
 
 }
