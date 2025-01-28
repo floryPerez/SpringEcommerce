ae975e9b8220bfad8948db5cfb3b56e91564b5a0
floryPerez <51434291+floryPerez@users.noreply.github.com>
2025-01-27 21:24:10 -0600

    Metodo para generar el numero de la orden

--- Documents/GitHub/Spring-ecommerce/src/main/java/com/curso/ecommerce/service/OrdenServiceImpl.java
+++ Documents/GitHub/Spring-ecommerce/src/main/java/com/curso/ecommerce/service/OrdenServiceImpl.java
@@ -1,5 +1,8 @@
 package com.curso.ecommerce.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +21,63 @@ public class OrdenServiceImpl implements IOrderService{
 	public Orden save(Orden orden) {
 		// TODO Auto-generated method stub
 		return ordenRepository.save(orden);
+	}
+
+	@Override
+	public List<Orden> findAll() {
+		// TODO Auto-generated method stub
+		return ordenRepository.findAll();
+	}
+	//crear metodo que muestra la secuencial de orden
+	
+	//OOOO10
+	public String generarNumeroOrden() {
+		
+		int numero =0;
+		
+		//incrementar el numero de la orden 
+		String numeroConcatenado="";
+		//crear lista
+		List<Orden> ordenes =findAll();
+		
+		//lista de enteros
+		List<Integer> numeros=new ArrayList<Integer>();
+		//poner el num de orden
+		
+		
+		ordenes.stream().forEach(o->numeros.add(Integer.parseInt(o.getNumero()
+				)));
+		
+		if(ordenes.isEmpty()) {
+			numero=1;
+			
+		}else {
+			numero=numeros.stream().max(Integer::compare).get();
+		numero++;
+		
+		}
+		
+		//pasar esto a cadena
+		
+		if(numero<10) {//0000000001
+			numeroConcatenado="000000000"+String.valueOf(numero);
+			
+		}
+		else if(numero<100) {
+			numeroConcatenado="00000000"+String.valueOf(numero);
+			
+		}
+		else if(numero<1000) {
+			numeroConcatenado="0000000"+String.valueOf(numero);
+			
+		}
+		else if(numero<10000) {
+			numeroConcatenado="0000000"+String.valueOf(numero);
+			
+		}
+		return numeroConcatenado;
+		
+		
 	}
 
 }
